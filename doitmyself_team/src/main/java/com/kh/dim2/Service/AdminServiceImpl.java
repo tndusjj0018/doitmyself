7ddcf32f75c4a3fc8fc2269f2088a2ff67ee9501
pinky <pinky@adorableU>
2020-01-15 18:31:42 +0900

    admin Controller, jsp, Service 수정 sql 수정 => sql문에 중복된 내용 지움

--- doitmyself_team/src/main/java/com/kh/dim2/Service/AdminServiceImpl.java
+++ doitmyself_team/src/main/java/com/kh/dim2/Service/AdminServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.kh.dim2.DAO.adminDAO;
 import com.kh.dim2.domain.Member;
+import com.kh.dim2.domain.Review;
 @Service
 public class AdminServiceImpl implements AdminService{
 	@Autowired
@@ -21,10 +22,29 @@ public class AdminServiceImpl implements AdminService{
 	}
 
 	@Override
-	public List<Member> getMemberList(int page, int limit) {
+	public List<Member> getMemberList(int page, int limit,String search_word) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("page", page);
 		map.put("limit", limit);
+		if(!search_word.equals("")) {
+			map.put("search_word", search_word);
+		}
 		return adminDAO.getMemberList(map);
+	}
+
+	@Override
+	public int getReviewCount() {
+		System.out.println("여기는 AdminServiceImpl의 getReviewCount()");
+		return adminDAO.getReviewCount();
+	}
+
+	@Override
+	public List<Review> getReviewList(int page, int limit, String orderBy) {
+		System.out.println("여기는 AdminServiceImpl의 getReviewList()");
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("page", page);
+		map.put("limit", limit);
+		map.put("orderBy", orderBy);
+		return adminDAO.getReviewList(map);
 	}
 }
