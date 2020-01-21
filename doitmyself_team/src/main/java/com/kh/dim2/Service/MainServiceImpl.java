6afc407582a3369e0a9272bcad8abbe4a830e42b
tndusjj0018 <57592008+tndusjj0018@users.noreply.github.com>
2020-01-21 19:32:24 +0900

    태양 0121

--- doitmyself_team/src/main/java/com/kh/dim2/Service/MainServiceImpl.java
+++ doitmyself_team/src/main/java/com/kh/dim2/Service/MainServiceImpl.java
@@ -19,6 +19,37 @@ public class MainServiceImpl implements MainService{
 
 	@Override
 	public int isId(String uSER_ID) {
-		return dao.idcheck(uSER_ID);
+		Member mb = dao.idcheck(uSER_ID);
+		return (mb == null) ? -1 : 1;
+	}
+
+	@Override
+	public int isEmail(String uSER_EMAIL) {
+		Member mb = dao.emailcheck(uSER_EMAIL);
+		return (mb == null) ? -1 : 1;
+	}
+
+	@Override
+	public int isId(String uSER_ID, String uSER_PASSWORD) {
+		Member mb = dao.login(uSER_ID , uSER_PASSWORD);
+		int result = -1;
+		if(mb != null) {
+			if(mb.getUSER_PASSWORD().equals(uSER_PASSWORD)) {
+				result = 1;
+			} else {
+				result = 0;
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public int isSeller(String uSER_ID) {
+		return dao.sellercheck(uSER_ID);
+	}
+
+	@Override
+	public Member isAdmin(String uSER_ID) {
+		return dao.admin_check(uSER_ID);
 	}
 }
