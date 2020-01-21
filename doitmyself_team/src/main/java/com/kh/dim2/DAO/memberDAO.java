af03b8e47c6a480639d569e7469d33eb3969e2b6
tndusjj0018 <57592008+tndusjj0018@users.noreply.github.com>
2020-01-21 18:41:06 +0900

    soo - 0121 1840

--- doitmyself_team/src/main/java/com/kh/dim2/DAO/memberDAO.java
+++ doitmyself_team/src/main/java/com/kh/dim2/DAO/memberDAO.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.dim2.domain.Member;
+import com.kh.dim2.domain.Order;
 import com.kh.dim2.domain.Product;
-import com.kh.dim2.domain.Qna;
+import com.kh.dim2.domain.Q_Product;
 import com.kh.dim2.domain.Seller;
 
 @Repository
@@ -66,11 +67,18 @@ public class memberDAO {
 		return sqlSession.update("Member.p_dibsupdate", p_no);
 	}
 
-	public List<Qna> qnalist(String user_id) {
+	public List<Q_Product> qnalist(String user_id){
 		return sqlSession.selectList("Member.qnalist", user_id);
 	}
-	
+		
 	public int qnacount(String user_id) {
 		return sqlSession.selectOne("Member.qnacount", user_id);
+	}
+	public List<Order> orderlist(String user_id){
+		return sqlSession.selectList("Member.orderlist", user_id);
+	}
+	
+	public int ordercount(String user_id) {
+		return sqlSession.selectOne("Member.ordercount", user_id);
 	}
 }
