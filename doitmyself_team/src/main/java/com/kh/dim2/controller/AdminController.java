1d96aa366688108cd913f41d7bf6aa3a55f07282
tndusjj0018 <57592008+tndusjj0018@users.noreply.github.com>
2020-01-21 10:23:52 +0900

    Update AdminController.java

--- doitmyself_team/src/main/java/com/kh/dim2/controller/AdminController.java
+++ doitmyself_team/src/main/java/com/kh/dim2/controller/AdminController.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,7 +31,8 @@ public class AdminController {
 	
 	//관리자 페이지로 이동
 	@GetMapping("admin")
-	public ModelAndView AdminPage(String doc, ModelAndView mv, HttpServletRequest request) {
+	public ModelAndView AdminPage(String doc, @RequestParam(value = "num", defaultValue = "1", required = false) int num, 
+			ModelAndView mv, HttpServletRequest request) {
 		System.out.println("view = "+ doc);
 		if(doc == null) {
 			doc = "userview";
@@ -41,6 +41,7 @@ public class AdminController {
 			mv.addObject("member", member);
 			
 		}
+		mv.addObject("num",num);
 		mv.addObject("doc",doc);
 		mv.setViewName("admin/admin");
 		
@@ -50,10 +51,13 @@ public class AdminController {
 	//유저 리스트 보기 ajax
 	@ResponseBody//Ajax쓸 때 꼭 써야함
 	@PostMapping("userList")
-	public Object userList(@RequestParam(value = "num", defaultValue = "1", required = false)int num, 
+	public Object userList(@RequestParam(value = "num", defaultValue = "1", required = false) int num, 
 							@RequestParam(value = "search_word", defaultValue = "", required = false)String search_word,
 							@RequestParam(value = "search_col",  defaultValue = "USER_ID",required = false) String search_col,
+							@RequestParam(value="option", required= false) String option,
 							HttpServletRequest request){
+		
+		System.out.println("search_word는  = " + search_word);
 		if(!search_word.equals("")) {
 			word = search_word;
 			//검색을 했을 때 처음 띄워줄 페이지는 1페이지
@@ -77,10 +81,10 @@ public class AdminController {
 			endpage = maxpage;
 		List<Member> memberlist;
 		if(search_word == "") {
-			memberlist = adminService.getMemberList(num, limit);
+			memberlist = adminService.getMemberList(num, limit, option);
 			System.out.println("모든 유저 리스트 보여주기");
 		}else {
-			memberlist = adminService.getMemberList(num, limit, word, search_col);
+			memberlist = adminService.getMemberList(num, limit, word, search_col, option);
 		}
 		
 		
@@ -94,7 +98,7 @@ public class AdminController {
 		map.put("limit", limit);
 		map.put("search_word", word);
 		map.put("search_col", search_col);
-		
+		map.put("option", option);
 		
 		return map;
 	}
@@ -123,22 +127,23 @@ public class AdminController {
 		out.close();
 	}
 	
-	@GetMapping("ModifyUser")
-	public void ModifyUser(HttpServletResponse response, int USER_NO)throws Exception {
+	@PostMapping("ModifyUser")
+	public void ModifyUser(HttpServletResponse response, HttpServletRequest request, Member member)throws Exception {
+		int USER_NO = Integer.parseInt(request.getParameter("USER_NO")); 
 		System.out.println("AdminController의 ModifyUser");
+		System.out.println("member의 번호는" + USER_NO);
+		System.out.println("member의 name은" + member.getUSER_NAME());
 		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out=  response.getWriter();
-		int result = adminService.ModifyUser(USER_NO);
-		out.println("<script>");
+		
+		member.setUSER_NO(USER_NO);
+		int result = adminService.ModifyUser(member);
+		String message = "정보 수정에 실패하였습니다.";
+		System.out.println("result = " + result);
+		
 		if(result == 1) {
-			out.println("alert('유저 정보 수정이 완료되었습니다.');");
-			out.println("location.href ='admin?doc=userview'");
-		}else {
-			out.println("alert('유저 정보 수정에 실패했습니다.');");
-			out.println("history.back();");
+			message = "정보 수정 성공";
 		}
-		out.println("</script>");
-		out.close();
+		response.getWriter().print(message);
 	}
 	
 	@GetMapping("user_info")
@@ -177,6 +182,18 @@ public class AdminController {
 	@PostMapping("resetstaticvalue")
 	public void ResetStaticValue() {
 		word = "";
+	}
+	
+	@PostMapping("admin_privilege_change")
+	public void privilege_change(int USER_NO,int USER_IS_ADMIN, HttpServletResponse response)throws Exception {
+		System.out.println("여기는 AdminController의 privilege_change");
+		int result = adminService.updateAdminPrivilege(USER_NO, USER_IS_ADMIN);
+		String message = "권한 수정 실패";
+		if(result == 1) {
+			message = "권한 수정 성공";
+		}
+		response.getWriter().print(message);
+
 	}
 	
 }
