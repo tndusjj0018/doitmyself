eab36e6b03a789262b1b572a5d66ae4904011edf
user1 <user1@DESKTOP-8UC660Q>
2020-01-22 17:55:15 +0900

    20-01-22 태양_장바구니 , DIM리스트 , 플로팅메뉴

--- doitmyself_team/src/main/java/com/kh/dim2/Service/MainService.java
+++ doitmyself_team/src/main/java/com/kh/dim2/Service/MainService.java
@@ -1,8 +1,12 @@
 package com.kh.dim2.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.kh.dim2.domain.Member;
+import com.kh.dim2.domain.Product;
+import com.kh.dim2.domain.Recent_View;
 
 @Service
 public interface MainService {
@@ -13,5 +17,10 @@ public interface MainService {
 	int isId(String uSER_ID, String uSER_PASSWORD);
 	int isSeller(String uSER_ID);
 	Member isAdmin(String uSER_ID);
+	List<Product> getBestProduct_List();
+	List<Product> getNewProduct_List();
+	int cartCount(String id);
+	int recentViewCount(String uSER_ID);
+	List<Recent_View> getRecent_View_List(String uSER_ID);
 
 }
