e53e104a2c14004c23854adf7943d4c002536b71
tndusjj0018 <57592008+tndusjj0018@users.noreply.github.com>
2020-02-01 00:40:04 +0900

    0131 예영 합치기

---
+++ doitmyself_team/src/main/java/com/kh/dim2/domain/Refund.java
@@ -0,0 +1,125 @@
+package com.kh.dim2.domain;
+
+public class Refund {
+	private String P_IMG;
+	private String P_NAME;	
+	private int ORDER_NO;
+	private int ORDER_P_NO;
+	private int ORDER_DELIVERY;
+	private int ORDER_STATUS;
+	private String ORDER_ID;
+	private int ORDER_CATEGORY;
+	private int ORDER_PRICE;
+	private String ORDER_PAYMENT;
+	private String ORDER_ADDRESS;
+	private String ORDER_DATE;
+	private String ORDER_R_DATE;
+	private String ORDER_SELLER; 
+	private String ORDER_TRNO;
+	private int ORDER_AMOUNT;
+	private int REFUND_STATUS;
+	public String getP_IMG() {
+		return P_IMG;
+	}
+	public void setP_IMG(String p_IMG) {
+		P_IMG = p_IMG;
+	}
+	public String getP_NAME() {
+		return P_NAME;
+	}
+	public void setP_NAME(String p_NAME) {
+		P_NAME = p_NAME;
+	}
+	public int getORDER_NO() {
+		return ORDER_NO;
+	}
+	public void setORDER_NO(int oRDER_NO) {
+		ORDER_NO = oRDER_NO;
+	}
+	public int getORDER_P_NO() {
+		return ORDER_P_NO;
+	}
+	public void setORDER_P_NO(int oRDER_P_NO) {
+		ORDER_P_NO = oRDER_P_NO;
+	}
+	public int getORDER_DELIVERY() {
+		return ORDER_DELIVERY;
+	}
+	public void setORDER_DELIVERY(int oRDER_DELIVERY) {
+		ORDER_DELIVERY = oRDER_DELIVERY;
+	}
+	public int getORDER_STATUS() {
+		return ORDER_STATUS;
+	}
+	public void setORDER_STATUS(int oRDER_STATUS) {
+		ORDER_STATUS = oRDER_STATUS;
+	}
+	public String getORDER_ID() {
+		return ORDER_ID;
+	}
+	public void setORDER_ID(String oRDER_ID) {
+		ORDER_ID = oRDER_ID;
+	}
+	public int getORDER_CATEGORY() {
+		return ORDER_CATEGORY;
+	}
+	public void setORDER_CATEGORY(int oRDER_CATEGORY) {
+		ORDER_CATEGORY = oRDER_CATEGORY;
+	}
+	public int getORDER_PRICE() {
+		return ORDER_PRICE;
+	}
+	public void setORDER_PRICE(int oRDER_PRICE) {
+		ORDER_PRICE = oRDER_PRICE;
+	}
+	public String getORDER_PAYMENT() {
+		return ORDER_PAYMENT;
+	}
+	public void setORDER_PAYMENT(String oRDER_PAYMENT) {
+		ORDER_PAYMENT = oRDER_PAYMENT;
+	}
+	public String getORDER_ADDRESS() {
+		return ORDER_ADDRESS;
+	}
+	public void setORDER_ADDRESS(String oRDER_ADDRESS) {
+		ORDER_ADDRESS = oRDER_ADDRESS;
+	}
+	public String getORDER_DATE() {
+		return ORDER_DATE;
+	}
+	public void setORDER_DATE(String oRDER_DATE) {
+		ORDER_DATE = oRDER_DATE;
+	}
+	public String getORDER_R_DATE() {
+		return ORDER_R_DATE;
+	}
+	public void setORDER_R_DATE(String oRDER_R_DATE) {
+		ORDER_R_DATE = oRDER_R_DATE;
+	}
+	public String getORDER_SELLER() {
+		return ORDER_SELLER;
+	}
+	public void setORDER_SELLER(String oRDER_SELLER) {
+		ORDER_SELLER = oRDER_SELLER;
+	}
+	public String getORDER_TRNO() {
+		return ORDER_TRNO;
+	}
+	public void setORDER_TRNO(String oRDER_TRNO) {
+		ORDER_TRNO = oRDER_TRNO;
+	}
+	public int getORDER_AMOUNT() {
+		return ORDER_AMOUNT;
+	}
+	public void setORDER_AMOUNT(int oRDER_AMOUNT) {
+		ORDER_AMOUNT = oRDER_AMOUNT;
+	}
+	public int getREFUND_STATUS() {
+		return REFUND_STATUS;
+	}
+	public void setREFUND_STATUS(int rEFUND_STATUS) {
+		REFUND_STATUS = rEFUND_STATUS;
+	}
+	
+	
+}
